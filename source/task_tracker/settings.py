ac112b887ec9b586d2d150d687f612c59cf08982
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-03-02 08:17:55 +0600

    Провел миграцию с выбором и переносом БД

--- source/task_tracker/settings.py
+++ source/task_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tracker',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'task_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "task_tracker_db",
+        "USER": "postgres",
+        "PASSWORD": "kuka2305",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
@@ -116,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
